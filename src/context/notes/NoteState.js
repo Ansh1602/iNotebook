d35cb5bfd20da3463e810b4d48ec977797365f3a
Ansh <ansh.khanna16@gmail.com>
2022-10-12 01:04:58 +0530

    Delete Note API done

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -49,8 +49,19 @@ const NoteState = (props) => {
   }
 
   // Delete a Note
-  const deleteNote = (id) => {
-    // TODO: API Call
+  const deleteNote = async (id) => {
+    // API Call 
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM0NWJmZjIyNTZkMTVmMzMxMmVkMGVhIn0sImlhdCI6MTY2NTUxNTY0Nn0.dD6CZsn86tLtSYkpIzoYj872-X8wLcMMQ9_0r4Fpa5Q'
+      },
+    });
+
+    const json = response.json();
+    console.log(json);
+    
     console.log("Deleting the note with id" + id);
     const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
